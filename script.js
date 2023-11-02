3ccabbc559aea8fd9898eee068009787a96abdb7
ImValkyrie <bitte_1992@hotmail.com>
2023-11-02 13:53:55 +0100

    Memory Game

--- script.js
+++ script.js
@@ -1,8 +1,62 @@
 const cards = document.querySelectorAll ('.memoryCards');
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipCard () {
-    console.log ('I was clicked');
-    console.log(this)
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+    //første klikk
+    hasFlippedCard = true;
+    firstCard = this;
+
+    return;
 }
+    //andre klikk
+    secondCard = this;
+
+    checkForMatch()
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unFlipCards();
+}
+
+function disableCards () {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unFlipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+    }, 1500);  
+}
+      
+function resetBoard () {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle(){
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 20);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
